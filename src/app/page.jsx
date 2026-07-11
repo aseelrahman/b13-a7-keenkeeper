229dee371622af6bd498095b9b3ff77c2c0968db
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-07-11 03:43:38 -0400

    complete home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
+import FriendCard from "@/component/FriendCard";
 import { FaPlus } from "react-icons/fa";
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch("http://localhost:3000/friends.json")
+  const users = await res.json()
+  
+  
   return (
-    <div className="bg-[#F8FAFC]">
+    <div className="bg-[#F8FAFC] py-20">
       <div className="container mx-auto min-h-screen">
-        <div className="flex flex-col items-center text-center pt-20">
+        <div className="flex flex-col items-center text-center">
           <h1 className="text-5xl font-bold">
             Friends to keep close in your life
           </h1>
@@ -19,7 +24,7 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10 m-10 sm:m-5">
           <div className="bg-[#FFFFFF] text-center flex flex-col gap-2 py-8 rounded-md shadow">
-            <span className="text-[32px] font-semibold">10</span>
+            <span className="text-[32px] font-semibold">{users.length}</span>
             <span className="text-[#64748B]">Total Friends</span>
           </div>
           <div className="bg-[#FFFFFF] text-center flex flex-col gap-2 py-8 rounded-md shadow">
@@ -36,7 +41,15 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="divider"></div>
+        <div className="border-t border-2 border-[#E9E9E9] mx-10 sm:mx-5 my-20"></div>
+
+        <h3 className="text-2xl font-semibold m-10 sm:m-5 text-center md:text-start">Your Friends</h3>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10 m-10 sm:m-5">
+          {
+            users.map(user => <FriendCard key={user.id} user={user}/>)
+          }
+        </div>
       </div>
     </div>
   );
--- src/component/FriendCard.jsx
+++ src/component/FriendCard.jsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
 import React from "react";
 
-const FriendCard = () => {
-  return <div></div>;
+const FriendCard = ({ user }) => {
+  const {
+    id,
+    name,
+    picture,
+    email,
+    days_since_contact,
+    status,
+    tags,
+    bio,
+    goal,
+    next_due_date,
+  } = user;
+  return (
+    <div className="bg-[#FFFFFF] text-center gap-2 flex flex-col py-8 rounded-md shadow">
+      <div className="flex justify-center">
+        <Image src={picture} alt="user image" width={100} height={100} className="rounded-full" />
+      </div>
+      <p className="text-[20px] font-semibold">{name}</p>
+      <span className="text-[12px] text-[#64748B]">{days_since_contact}d ago</span>
+      <ul className="flex justify-center gap-2">
+        {tags.map((tag, index) => {
+          return(<li key={index} className="text-[#244D3F] bg-[#CBFADB] p-2 rounded-[100px] font-medium">{tag.toUpperCase()}</li>);
+        })}
+      </ul>
+      <span className={`text-white ${status === 'overdue' ? 'bg-[#EF4444]' : status === 'on-track' ? 'bg-[#244D3F]' : status === 'almost due' ? 'bg-[#EFAD44]' : ''} p-2 rounded-[100px] font-medium capitalize mx-auto`}>{status}</span>
+    </div>
+  );
 };
 
 export default FriendCard;
